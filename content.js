4a36873f729424f5d9ecb4d0ec15d2a369e71182
Kolli Vishal Reddy <kvishalr@Vishals-MacBook-Pro.local>
2019-04-29 05:50:47 -0400

    UI changes

--- content.js
+++ content.js
@@ -1,15 +1,16 @@
 const url = chrome.runtime.getURL('data/data.json');
+const imgPop = chrome.runtime.getURL('img/pop.png');
 
 fetch(url)
-    .then((response) => response.json())
-    .then((json) => readJSON(json))
+.then((response) => response.json())
+.then((json) => readJSON(json))
 
-    function readJSON(json){
+function readJSON(json){
 
-    	//Toggle function
         setTimeout( function(){
 
         //Append IDs for each Tweet
+        
         var keys = [];
         for (property in json["all_tweets"]) {
             keys.push(property);
@@ -20,38 +21,46 @@ fetch(url)
             i = i+1;
         });
 
-    	for (property in json["all_tweets"]) {
-    		//This is static for now, needs to be changed to a keyword or hashtag that we search for each tweet
+        //Fetch relevant from different sources for a single tweet
+
+        for (property in json["all_tweets"]) {
 
             for (var j=0;j<keys.length;j++)
             {
-    		if(property === keys[j]){
-    			for (var i =0; i< json["all_tweets"][property].length; i++){
+              if(property === keys[j]){
+                 for (var i =0; i< json["all_tweets"][property].length; i++){
 
-    				var elm = '<div class="tweet testing item">' +
-    				json["all_tweets"][property][i]["tweet"] + 
-    				'<br /><a href="' + json["all_tweets"][property][i]["link"] + 
-    				'">Click to View</a> | ' + json["all_tweets"][property][i]["user"] +
-    				'</div>';
+                    var elm = '<div class="tweet testing item"><div class="row"> ' + 
+                    '<div class="column"><img class="imgPop" text-align="right" src="' + json["all_tweets"][property][i]["photo"] + '" /></div>' +
+                    '<div class="double-column">' +
+                    '<strong>' + json["all_tweets"][property][i]["user"] +  ' </strong> <a href="' + json["all_tweets"][property][i]["link"] + 
+                    '">View</a>'+ 
+                    json["all_tweets"][property][i]["tweet"] + 
+                    '</div>'  + 
+                    '</div></div>';
                     console.log(keys[j]);
-    				$(elm).insertAfter('.' + keys[j]);
+                    $(elm).insertAfter('.' + keys[j]);
                     $(".testing").hide();
-    			}
-    		}
+                }
+
+
             }
-    	}
+        }
+    }
 
         $(".r-1g40b8q h2").text("Pop Feed");
-    	//Pop Link
- 
-            var popLink = '<a class="pop" href="#">Pop It!</a>';
-            $("article").append(popLink);
 
-    	$(".pop").click(function(e) {
-	    e.preventDefault();
-	    $(".testing").toggle();
-		});
-        }, 1000);
+    
+    	//Pop Link
+
+        var popLink = '<div><a class="pop width-150px" href="#" align="center">Pop</a></div>';
+        $("article").append(popLink);
+
+        $(".pop").click(function(e) {
+           e.preventDefault();
+           $(".testing").toggle();
+       });
+    }, 2000);
 
 
     }
